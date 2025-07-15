c955c2538df455f75bec633f8176d7fe2b73ecbb
Nguyễn Lê Ngọc Duy <ngocdilearnds@gmail.com>
2025-07-15 12:57:31 +0700

    [UPDATE] Fix the code

--- src/agent/configs.py
+++ src/agent/configs.py
@@ -7,3 +7,6 @@ load_dotenv(dotenv_path="/src/configs/.env",verbose=True, override=True)
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
+BASE_URL = os.getenv("BASE_URL")
+REQUEST_TIMEOUT_SECONDS = os.getenv("REQUEST_TIMEOUT_SECONDS")
+RATE_LIMIT_URI = os.getenv("RATE_LIMIT_URI")
--- src/agent/schema/message.py
+++ src/agent/schema/message.py
@@ -5,12 +5,6 @@ class BaseMessageSchema(BaseModel):
     role: str = Field(..., description="Role of the message sender (e.g., 'user', 'assistant', 'system')")
     content: str = Field(..., description="Content of the message")
 
-    def to_dict(self) -> dict:
-        return {
-            "role": self.role,
-            "content": self.content
-        }
-
 
 class Message(BaseMessageSchema):
     pass
